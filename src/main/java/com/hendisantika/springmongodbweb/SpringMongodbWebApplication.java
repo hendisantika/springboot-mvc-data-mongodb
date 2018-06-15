c1680854735fd65b0d07da3920a061c65838db07
Hendi Santika <hendisantika@gmail.com>
2018-06-16 05:55:31 +0700

    #23 - Add Intialize Data

--- src/main/java/com/hendisantika/springmongodbweb/SpringMongodbWebApplication.java
+++ src/main/java/com/hendisantika/springmongodbweb/SpringMongodbWebApplication.java
@@ -1,7 +1,11 @@
 package com.hendisantika.springmongodbweb;
 
+import com.hendisantika.springmongodbweb.model.Product;
+import com.hendisantika.springmongodbweb.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 /**
@@ -20,5 +24,16 @@ public class SpringMongodbWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMongodbWebApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initialize(ProductRepository repo) {
+        return args -> {
+            repo.deleteAll();
+            repo.save(new Product("Intel Core i3", "Intel Core i3 Processor", 200.0, "https://www.links.hr/content/images/thumbs/004/0044431_procesor-intel-core-i3-8100-s-1151-3-6ghz-6mb-cache-gpu-quad-core-050600063.png"));
+            repo.save(new Product("Intel Core i5", "Intel Core i4 Processor", 200.0, "http://www.murdockcruz.com/wp-content/uploads/2017/08/Intel-Core-i5-7200U-3.jpg"));
+            repo.save(new Product("Intel Core i7", "Intel Core i7 Processor", 200.0, "https://www.notebookcheck.net/uploads/tx_nbc2/corei7_09.png"));
+        };
+
     }
 }
